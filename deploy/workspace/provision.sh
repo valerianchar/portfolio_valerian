9ddf89603d3eea98fbf69a8bb8f0ea227204b292
valerianchar <valerian.charrier@gmail.com>
2025-05-20 11:15:29 +0200

    version 1.0.28

--- deploy/workspace/provision.sh
+++ deploy/workspace/provision.sh
@@ -38,23 +38,12 @@ chmod -R 777 /var/www/storage
 chown -R 1000:1000 /var/www/storage
 
 sed -i "s~$(grep -e "APP_DEBUG=" .env)~APP_DEBUG=${APP_DEBUG}~g" .env
-#sed -i "s~$(grep -e "APP_URL=" .env)~APP_URL=${APP_URL}~g" .env
-#
-#sed -i "s~$(grep -e "ASSET_URL=" .env)~ASSET_URL=${ASSET_URL}~g" .env
-#
-#sed -i "s~$(grep -e "DB_CONNECTION=" .env)~DB_CONNECTION=${DB_CONNECTION}~g" .env
-#sed -i "s~$(grep -e "DB_HOST=" .env)~DB_HOST=${DB_HOST}~g" .env
-#sed -i "s~$(grep -e "DB_PORT=" .env)~DB_PORT=${DB_PORT}~g" .env
-#sed -i "s~$(grep -e "DB_DATABASE=" .env)~DB_DATABASE=${DB_DATABASE}~g" .env
-#sed -i "s~$(grep -e "DB_USERNAME=" .env)~DB_USERNAME=${DB_USERNAME}~g" .env
-#sed -i "s~$(grep -e "DB_PASSWORD=" .env)~DB_PASSWORD=${DB_PASSWORD}~g" .env
 
 php artisan key:generate
 php artisan clear-compiled
 php artisan view:cache
 php artisan route:cache
 php artisan config:cache
-php artisan key:generate
 
 php artisan migrate --force
 
